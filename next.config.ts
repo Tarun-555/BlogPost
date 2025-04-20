f18d99d866e49840970229e2f16ba3486b282cc4
Tarun <trn18r@gmail.com>
2025-04-20 21:45:22 +0530

    fix: images display issue

--- next.config.ts
+++ next.config.ts
@@ -9,8 +9,13 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "**",
       },
+      {
+        protocol: "https",
+        hostname: "media.gettyimages.com**",
+      },
     ],
     domains: [
+      "*",
       "media.gettyimages.com",
       "avatars.githubusercontent.com",
       "https://vocal-tarsier-7f6e8b.netlify.app",
